e5f0cb2223248402ce497588078c8436d7eb283b
Rinko <1957880233@qq.com>
2019-08-04 15:11:00 +0800

    MyFragment完毕

--- MyFragment/app/src/main/java/com/example/myfragment/TestFragment.java
+++ MyFragment/app/src/main/java/com/example/myfragment/TestFragment.java
@@ -1,6 +1,7 @@
 package com.example.myfragment;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,11 +14,39 @@ import javax.security.auth.login.LoginException;
 public class TestFragment extends Fragment {
 
     public static final String TAG = TestFragment.class.getSimpleName();
+    public static final String ARGUMENT_NAME = "argument_name";
+    public static final String ARGUMENT_NUMBER = "argument_number";
+    private String name;
+    private int number;
+
+
+    public static TestFragment newInstance(String nameString, int number) {
+        Bundle bundle = new Bundle();
+        bundle.putString(ARGUMENT_NAME, nameString);
+        bundle.putInt(ARGUMENT_NUMBER, number);
+        TestFragment fragment = new TestFragment();
+        //该bundle中的数据与返回的fragment绑定，后续可以使用
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
+
+        //获得初始化的数据
+        Bundle bundle = this.getArguments();
+        if(bundle != null){
+            name = bundle.getString(ARGUMENT_NAME);
+            number = bundle.getInt(ARGUMENT_NUMBER);
+        }
     }
 
     @Override
@@ -27,8 +56,13 @@ public class TestFragment extends Fragment {
         //创建一个view，不绑定到视图中
         View view = inflater.inflate(R.layout.item_phone_book_friend, container ,false);
         TextView nameTextView = view.findViewById(R.id.name_text_view);
-        nameTextView.setText("fragment");
+        nameTextView.setText(name);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 
     @Override
--- MyFragment/app/src/main/java/com/example/myfragment/TestFragmentActivity.java
+++ MyFragment/app/src/main/java/com/example/myfragment/TestFragmentActivity.java
@@ -8,30 +8,65 @@ import android.os.Bundle;
 
 
 public class TestFragmentActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_test);
 
-        //add，添加Fragment
+        /**
+         * add，添加Fragment
+         */
         //获得Fragment控制器，和用于执行的秘书！控制器是老总，有一个秘书FragmentTransaction负责执行
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        //新建一个Fragment。
+
+        /**
+         * 新建一个Fragment。
+         */
         // 注意fragment在OnCreateView中已有过处理，但是未与父控件绑定，故能够放入新的布局中
         TestFragment testFragment = new TestFragment();
-        //将该Fragment放入到fragment_view这个线性布局中，并执行！此时，一个Fragment已经完成
-        fragmentTransaction.add(R.id.fragment_view, testFragment);
+//        TestFragment testFragment = TestFragment.newInstance("我是阿泰", 21);
+//        将该Fragment放入到fragment_view这个线性布局中，并执行！此时，一个Fragment已经完成
+        fragmentTransaction.add(R.id.fragment_view, testFragment, "test_fragment_tag_1").commit();
 
-        //删除
-        fragmentTransaction.remove(testFragment).commit();
+        /**
+         *删除
+         */
+//        fragmentTransaction.remove(testFragment).commit();
 
-        //找到一个已有的Fragment
-        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_test_1);
-        if(fragment instanceof TestFragment){
-            //TODO: DO YOUR ACTION
-        }else{
-            throw new IllegalStateException("is not testFragment!");
-        }
+        /**
+         * 查找
+         */
+//        //找到一个已有的Fragment，也可按Tag来查找，只要之前在add是加入tag参数
+//        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_test_1);
+//        /**
+//         * 总是返回null！！！
+//         */
+//        fragment = fragmentManager.findFragmentByTag("test_fragment_tag_1");
+//        if(fragment == null){
+//            throw new IllegalStateException("null!");
+//        }
+//        if(fragment instanceof TestFragment){
+//            //TODO: DO YOUR ACTION
+//        }else{
+//            throw new IllegalStateException("is not testFragment!");
+//        }
+
+        fragmentManager.addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                //当按下backspace时
+            }
+        });
+
+    }
+
+    @Override
+    /**
+     * 拿下Backspace时的动作
+     */
+    public void onBackPressed() {
+        super.onBackPressed();//默认删除该activity
     }
 }
